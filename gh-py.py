c990d5ed4faa9ef49cd6e4ea219b24405d154d0c
r-racheeel <113764045+r-racheeel@users.noreply.github.com>
2024-10-03 15:17:01 +0800

    first commit

---
+++ gh-py.py
@@ -0,0 +1,15 @@
+# import base64
+# import os
+
+# class Text(BaseModel):
+
+# text: str
+
+# # Create a FastAPI endpoint that accepts a POST request with a JSON body containing a single field called "text" and returns a checksum of the text
+
+# convert number to string - str()
+# convert string to number - int()
+
+from datetime import date
+date.today()
+print(date.today())
